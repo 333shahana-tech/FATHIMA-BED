2599bbae2a548839bdfdbf315f78cadc403ba9c4
333shahana-tech <333shahana@gmail.com>
2026-04-13 16:24:54 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,156 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-safuwa01",
+      "name": "SAFUWA01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.3791769246529526,
+        "pitch": -0.09220633710944348,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8881346884183721,
+          "pitch": 0.4662134378331899,
+          "rotation": 7.0685834705770345,
+          "target": "1-safuwa02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-safuwa02",
+      "name": "SAFUWA02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0.0029456560518568153,
+          "rotation": 1.5707963267948966,
+          "target": "2-safuwa03"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-safuwa03",
+      "name": "SAFUWA03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 4.71238898038469,
+          "target": "3-safuwa04"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-safuwa04",
+      "name": "SAFUWA04",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
